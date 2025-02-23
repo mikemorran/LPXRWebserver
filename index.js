e219ec5965e5a6359001d5cb83085a0a4946b045
Michael Morran <67967136+mikemorran@users.noreply.github.com>
2025-02-23 16:12:26 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const { Server } = require("socket.io");
+const express = require("express");
+const http = require("http");
+const app = express();
+app.use(express.json());
+const server = http.createServer(app);
+const io = new Server(server);
+
+let touchDesignerSocket = null;
+
+io.on("connection", (socket) => {
+    console.log('New socket connection:', socket.id);
+
+    // Identify if a TouchDesigner client connects
+    socket.on('registerTouchDesigner', () => {
+        console.log('TouchDesigner client registered:', socket.id);
+        touchDesignerSocket = socket; // Store the socket
+    });
+
+    socket.on('loadPrompt', (msg) => {
+        console.log('loadPrompt:', msg);
+
+        // Send update to TouchDesigner client
+        if (touchDesignerSocket) {
+            touchDesignerSocket.emit('touchdesignerPrompt', msg);
+        }
+    });
+
+    socket.on('updatePrompt', (msg) => {
+        console.log('updatePrompt:', msg);
+
+        // Send update to TouchDesigner client
+        if (touchDesignerSocket) {
+            touchDesignerSocket.emit('touchdesignerPrompt', msg);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        if (socket === touchDesignerSocket) {
+            console.log('TouchDesigner client disconnected');
+            touchDesignerSocket = null;
+        }
+    });
+});
+
+app.get("/", (req, res) => {
+    res.send("Server is up and running!");
+});
+
+const PORT = process.env.PORT || 4040;
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
